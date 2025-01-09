b5e56f0ef88a0e94515b02bb49a5ef172c1c613e
antchristensen <anthony.christensen1988@gmail.com>
2025-01-09 13:44:21 -0700

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
-// Set the current year in the first paragraph
+
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-// Set the last modified date in the second paragraph
+
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
