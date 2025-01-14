974df315d778b1be206d3dfb5b030cd2f38d3912
antchristensen <anthony.christensen1988@gmail.com>
2025-01-14 10:43:35 -0700

    bom

---
+++ week02/scripts/bom.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('')
+
+const li = document.createElement('li')
+const deletebutton = document.createElement('button');
+
+li.textContent = input.ariaValueMax;
+
+deletebutton.textContent = '❌';
+
+li.append(deleteButton);
+
+list.append(li);
