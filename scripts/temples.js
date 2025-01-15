a2556c3af939b81a051e27b5f7c06a7a587a0e81
antchristensen <anthony.christensen1988@gmail.com>
2025-01-14 19:22:57 -0700

    last modified added

--- scripts/temples.js
+++ scripts/temples.js
@@ -27,3 +27,15 @@ document.addEventListener("DOMContentLoaded", () => {
     window.dispatchEvent(new Event("resize"));
   });
   
+  const today = new Date();
+
+const currentYear = document.querySelector("#currentYear")
+const lastModified = document.querySelector("#lastModified")
+
+
+const fullYear = today.getFullYear()
+
+const lastModification = document.lastModified
+
+currentYear.textContent = fullYear
+lastModified.textContent = `Last Modification: ${lastModification}`
